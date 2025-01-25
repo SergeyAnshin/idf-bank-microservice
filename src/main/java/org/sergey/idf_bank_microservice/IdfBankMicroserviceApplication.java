261ce222f7dbb49833ad9cba1edd16e422d86ad3
Sergey <anshin.serega@gmail.com>
2025-01-25 18:31:58 +0300

    create init gitignore, application.yml, mvn, pom and java files

---
+++ src/main/java/org/sergey/idf_bank_microservice/IdfBankMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.sergey.idf_bank_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfBankMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdfBankMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sergey/idf_bank_microservice/IdfBankMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergey.idf_bank_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfBankMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
